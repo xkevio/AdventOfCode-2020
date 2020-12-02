819c9ed94be2068c8514ba9ec0587f8eb3edc6b8
xkevio <xkeviota@gmail.com>
2020-12-03 00:58:43 +0100

    removed unnecessary include

--- day_2/main.cpp
+++ day_2/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <regex>
 
 bool check_if_password(std::string word, char letter, int min, int max, bool part2) {
     if(part2) return word[min-1] == letter ^ word[max-1] == letter;
