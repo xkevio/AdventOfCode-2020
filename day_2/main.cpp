3a88ae882380a7f0160b31ea0dcc50c3f312de27
xkevio <xkeviota@gmail.com>
2020-12-03 03:35:49 +0100

    changed solution to a one-liner because why not

--- day_2/main.cpp
+++ day_2/main.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 bool check_if_password(std::string word, char letter, int min, int max, bool part2) {
-    if(part2) return word[min-1] == letter ^ word[max-1] == letter;
-
-    int a = 0;
-    for (size_t i = 0; i < word.length(); i++)
-    {
-        if(word[i] == letter) {
-            a++;
-        }
-    }
-    return a >= min && a <= max;
+    return part2 ? word[min-1] == letter ^ word[max-1] == letter : std::count(word.begin(), word.end(), letter) >= min && std::count(word.begin(), word.end(), letter) <= max;
 }
 
 int main(int, char**) {
