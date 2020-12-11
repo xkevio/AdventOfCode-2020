3019a0110bde3f0c29352422eb3ef586040a1711
xkevio <xkeviota@gmail.com>
2020-12-11 19:39:26 +0100

    Added day 11 challenge, part 2 can be reworked

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ This repo is a work-in-progress to fully cover all challenges. So have fun with 
 | ------------- |:-------------:|
 | ✅ Day 1    | ✅ Day 8 | 
 | ✅ Day 2    | ✅ Day 9   |   
-| ✅ Day 3 | ✅ Day 10
-| ✅ Day 4 |
+| ✅ Day 3 | ✅ Day 10 |
+| ✅ Day 4 | ✅ Day 11 |
 | ✅ Day 5 |
 | ✅ Day 6 |
 | ✅ Day 7    |      |   
---
+++ day_11/main.cpp
@@ -0,0 +1,199 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+int get_vision_neighbours(int row, int col, const std::vector<std::vector<char>> &grid) {
+    int n = 0;
+    int width = grid[0].size();
+    int height = grid.size();
+    
+    int i = 1;
+    int j = 1;
+    while(row - i >= 0) {
+        if(grid[row-i][col] == '#') {
+            n++;
+            break;
+        } 
+        if(grid[row-i][col] == 'L') {
+            break;
+        }
+        i++;
+    }
+    i = 1;
+    j = 1;
+    while(row - i >= 0  && col - i >= 0) {
+        if(grid[row-i][col-i] == '#') {
+            n++;
+            break;
+        } 
+        if(grid[row-i][col-i] == 'L') {
+            break;
+        }
+        i++;
+    }
+    i = 1;
+    j = 1;
+    while(row - i >= 0 && col + j < width ) {
+        if(grid[row-i][col+j] == '#') {
+            n++;
+            break;
+        } 
+        if(grid[row-i][col+j] == 'L') {
+            break;
+        }
+        i++;
+        j++;
+    }
+    i = 1;
+    j = 1;
+    while(col - i >= 0) {
+        if(grid[row][col-i] == '#') {
+            n++;
+            break;
+        } 
+        if(grid[row][col-i] == 'L') {
+            break;
+        }
+        i++;
+    }
+    i = 1;
+    j = 1;
+    while(col - i >= 0 && row + j < height) {
+        if(grid[row+j][col-i] == '#') {
+            n++;
+            break;
+        } 
+        if(grid[row+j][col-i] == 'L') {
+            break;
+        }
+        i++;
+        j++;
+    }
+    i = 1;
+    j = 1;
+    while(row + i < height) {
+        if(grid[row+i][col] == '#') {
+            n++;
+            break;
+        } 
+        if(grid[row+i][col] == 'L') {
+            break;
+        }
+        i++;
+    }
+    i = 1;
+    j = 1;
+    while(row + i < height && col + j < width) {
+        if(grid[row+i][col+j] == '#') {
+            n++;
+            break;
+        } 
+        if(grid[row+i][col+j] == 'L') {
+            break;
+        }
+        i++;
+        j++;
+    }
+    i = 1;
+    j = 1;
+    while(col + i < width) {
+        if(grid[row][col+i] == '#') {
+            n++;
+            break;
+        } 
+        if(grid[row][col+i] == 'L') {
+            break;
+        }
+        i++;
+    }
+
+
+    return n;
+}
+
+int get_neighbours(int row, int col, const std::vector<std::vector<char>> &grid) {
+    int n = 0;
+    int width = grid[0].size();
+    int height = grid.size();
+
+    if(row - 1 >= 0 && grid[row-1][col] == '#') n++;
+    if(row - 1 >= 0  && col - 1 >= 0 && grid[row-1][col-1] == '#') n++;
+    if(row - 1 >= 0 && col + 1 < width && grid[row-1][col+1] == '#') n++;
+    if(col - 1 >= 0 && grid[row][col - 1] == '#') n++;
+    if(col - 1 >= 0 && row + 1 < height && grid[row+1][col-1] == '#') n++;
+    if(row + 1 < height && grid[row+1][col] == '#') n++;
+    if(row + 1 < height && col + 1 < width && grid[row+1][col+1] == '#') n++;
+    if(col + 1 < width && grid[row][col+1] == '#') n++;
+
+    return n;
+}
+
+void cycle(const std::vector<std::vector<char>> &grid, std::vector<std::vector<char>> &new_grid, bool part2) {
+    for (size_t i = 0; i < grid.size(); i++)
+    {
+        for (size_t j = 0; j < grid[i].size(); j++)
+        {
+            if(!part2) {
+                if(grid[i][j] == 'L' && get_neighbours(i, j, grid) == 0) {
+                    new_grid[i][j] = '#';
+                }
+                if(grid[i][j] == '#' && get_neighbours(i, j, grid) >= 4) {
+                    new_grid[i][j] = 'L';
+                }
+            } else {
+                if(grid[i][j] == 'L' && get_vision_neighbours(i, j, grid) == 0) {
+                    new_grid[i][j] = '#';
+                }
+                if(grid[i][j] == '#' && get_vision_neighbours(i, j, grid) >= 5) {
+                    new_grid[i][j] = 'L';
+                }
+            }
+        }
+    }
+}
+
+int get_occupied_seats(std::vector<std::vector<char>> grid, bool part2) {
+    int seats = 0;
+    
+    auto grid2 = grid;
+    auto &new_grid = grid;
+    auto &new_grid2 = grid2;
+
+    cycle(grid2, new_grid, part2);
+    cycle(new_grid, new_grid2, part2);
+
+    while(new_grid != new_grid2) {
+        new_grid = new_grid2;
+        cycle(new_grid, new_grid2, part2);
+    }
+    for(auto k : new_grid) {
+        for(auto v : k) {
+            if(v == '#') seats++;
+        }
+    }
+
+    return seats;
+}
+
+int main(int, char**) {
+    std::ifstream file;
+    std::vector<std::vector<char>> grid;
+    std::string cur;
+
+    file.open("input.txt");
+    while(getline(file, cur)) {
+        std::vector<char> row;
+        for (size_t i = 0; i < cur.length(); i++)
+        {
+            row.push_back(cur[i]);
+        }
+        grid.push_back(row);
+    }
+
+    std::cout << "Part 1: " << get_occupied_seats(grid, false) << std::endl;
+    std::cout << "Part 2: " << get_occupied_seats(grid, true) << std::endl;
+
+    file.close();
+    return EXIT_SUCCESS;
+}
