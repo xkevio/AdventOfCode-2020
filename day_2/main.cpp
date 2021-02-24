0d7797c4748370aa15991cd92858de19db118a3c
xkevio <xkeviota@gmail.com>
2021-02-24 09:32:23 +0100

    fixed formatting and removed unused variables

--- day_2/main.cpp
+++ day_2/main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
 
-bool check_if_password(std::string word, char letter, int min, int max, bool part2) {
-    return part2 ? word[min-1] == letter ^ word[max-1] == letter : std::count(word.begin(), word.end(), letter) >= min && std::count(word.begin(), word.end(), letter) <= max;
+bool check_if_password(const std::string& word, char letter, int min, int max, bool part2) {
+    return part2 ? word[min - 1] == letter ^ word[max - 1] == letter : std::count(word.begin(), word.end(), letter) >= min && std::count(word.begin(), word.end(), letter) <= max;
 }
 
 int main(int, char**) {
@@ -16,18 +16,16 @@ int main(int, char**) {
 
     file.open("input.txt");
 
-    if(file.is_open()) {
-        while(std::getline(file, text)) {
+    if (file.is_open()) {
+        while (std::getline(file, text)) {
             int min = std::stoi(text.substr(0, text.find("-")));
             int max = std::stoi(text.substr(text.find("-") + 1, text.find(" ")));
-            int a = 0;
-            int b = 0;
 
             char letter = text.at(text.find(":") - 1);
             std::string word = text.substr(text.find(":") + 2);
 
-            if(check_if_password(word, letter, min, max, false)) p1++;
-            if(check_if_password(word, letter, min, max, true)) p2++;            
+            if (check_if_password(word, letter, min, max, false)) p1++;
+            if (check_if_password(word, letter, min, max, true)) p2++;
         }
     }
 
