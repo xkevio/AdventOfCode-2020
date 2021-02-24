1238bc59823d432a680d593aa9551b8e9facc48b
xkevio <xkeviota@gmail.com>
2021-02-24 09:24:52 +0100

    changed macro to constexpr and changed formatting

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -1,34 +1,31 @@
-#include <iostream>
+#include <algorithm>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
-#define SUM 2020
+constexpr int sum = 2020;
 
-//part 2
-int find_three_sum(std::vector<int> list) {
+// part 2
+int find_three_sum(const std::vector<int>& list) {
     int result = 0;
 
-    for (size_t i = 0; i < list.size() - 2; i++)
-    {
+    for (size_t i = 0; i < list.size() - 2; i++) {
         int ind = list[i];
-        int l = i+1;
+        int l = i + 1;
         int r = list.size() - 1;
 
-        while(l < r) {
-            if((list[l] + list[r] + ind) < SUM) {
+        while (l < r) {
+            if ((list[l] + list[r] + ind) < sum) {
                 l++;
-            } else if((list[l] + list[r] + ind) > SUM) {
+            } else if ((list[l] + list[r] + ind) > sum) {
                 r--;
             } else {
                 result = list[l] * list[r] * ind;
                 break;
             }
         }
-
     }
-    
 
     return result;
 }
@@ -40,30 +37,29 @@ int main(int, char**) {
 
     file.open("input.txt");
 
-    if(file.is_open()) {
-        while(std::getline(file, text)) {
-            list.push_back(atoi(text.c_str()));
+    if (file.is_open()) {
+        while (std::getline(file, text)) {
+            list.push_back(stoi(text));
         }
     }
     std::sort(list.begin(), list.end());
 
-    //part 1
+    // part 1
     int result = 0;
     int l = 0, r = list.size() - 1;
-    while(l < r) {
-        if((list[l] + list[r]) < SUM) {
+    while (l < r) {
+        if ((list[l] + list[r]) < sum) {
             l++;
-        } else if((list[l] + list[r]) > SUM) {
+        } else if ((list[l] + list[r]) > sum) {
             r--;
         } else {
             result = list[l] * list[r];
-            break; 
+            break;
         }
     }
 
     std::cout << "Part 1: " << result << std::endl;
     std::cout << "Part 2: " << find_three_sum(list) << std::endl;
-
 
     file.close();
     return EXIT_SUCCESS;
