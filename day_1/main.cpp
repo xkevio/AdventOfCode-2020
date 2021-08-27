74c95eef6a7e94f656402fddb4c413d76d3918a3
xkevio <xkeviota@gmail.com>
2021-08-27 23:10:13 +0200

    Clean up day 1

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -6,11 +6,28 @@
 
 constexpr int sum = 2020;
 
+// part 1
+int part1(const std::vector<int>& list) {
+    int result = 0;
+    int l = 0, r = list.size() - 1;
+    while (l < r) {
+        if ((list[l] + list[r]) < sum) {
+            l++;
+        } else if ((list[l] + list[r]) > sum) {
+            r--;
+        } else {
+            result = list[l] * list[r];
+            break;
+        }
+    }
+    return result;
+}
+
 // part 2
 int find_three_sum(const std::vector<int>& list) {
     int result = 0;
 
-    for (size_t i = 0; i < list.size() - 2; i++) {
+    for (size_t i = 0; i < list.size() - 2; ++i) {
         int ind = list[i];
         int l = i + 1;
         int r = list.size() - 1;
@@ -30,37 +47,21 @@ int find_three_sum(const std::vector<int>& list) {
     return result;
 }
 
-int main(int, char**) {
-    std::ifstream file;
+int main() {
+    std::ifstream file("input.txt");
     std::string text;
     std::vector<int> list;
 
-    file.open("input.txt");
-
-    if (file.is_open()) {
+    if (file) {
         while (std::getline(file, text)) {
             list.push_back(stoi(text));
         }
     }
+
     std::sort(list.begin(), list.end());
 
-    // part 1
-    int result = 0;
-    int l = 0, r = list.size() - 1;
-    while (l < r) {
-        if ((list[l] + list[r]) < sum) {
-            l++;
-        } else if ((list[l] + list[r]) > sum) {
-            r--;
-        } else {
-            result = list[l] * list[r];
-            break;
-        }
-    }
-
-    std::cout << "Part 1: " << result << std::endl;
+    std::cout << "Part 1: " << part1(list) << std::endl;
     std::cout << "Part 2: " << find_three_sum(list) << std::endl;
 
-    file.close();
     return EXIT_SUCCESS;
 }
